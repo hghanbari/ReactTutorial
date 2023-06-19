f617da0bf3e232ec7e16736f7db6b343667b57ec
hghanbari <gh.hamid89@gmail.com>
2023-06-19 18:11:54 +0200

    Section2 State & Handle Events

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  function formatName(userName) {
-    return userName.toUpperCase();
-  }
+import Clock from "./Clock";
 
-  function getGreeting(userName) {
-    if (userName) return <h1>Hello {formatName(userName)}</h1>;
-    else {
-      return <h1>Hello Stranger</h1>;
-    }
-  }
-  const name = "Hamid Ghanbari";
-  const element = <h1>Hello {formatName(name)} !</h1>;
-
-  return getGreeting("Jang");
+function App(props) {
+  return (
+    <div>
+      <Clock />
+    </div>
+  );
 }
 
 export default App;
