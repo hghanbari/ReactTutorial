df79f31309a606adedd0494a5fecd08db949ebb3
hghanbari <gh.hamid89@gmail.com>
2023-06-30 17:28:02 +0200

    Create post details page

--- react-awesome-blog/src/components/Navbar.js
+++ react-awesome-blog/src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../ThemeContext";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const { theme, toggleTheme } = useContext(ThemeContext);
   return (
     <div className="header">
       <div className="header-item">
-        <a href="/">
+        <Link to="/">
           <strong>Awesome Blog</strong>
-        </a>
+        </Link>
       </div>
       <div className="header-item">
         <a href="/login">Login</a>
--- react-awesome-blog/src/components/pages/HomePage.js
+++ react-awesome-blog/src/components/pages/HomePage.js
@@ -1,25 +1,46 @@
 import axios from "axios";
+import { Link } from "react-router-dom";
 import React, { useEffect, useReducer } from "react";
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "POSTS_REQUEST":
-      return { ...state, loading: true };
+      return { ...state, loadingPosts: true };
     case "POSTS_SUCCESS":
-      return { ...state, loading: false, posts: action.payload, error: "" };
+      return {
+        ...state,
+        loadingPosts: false,
+        posts: action.payload,
+        errorPosts: "",
+      };
     case "POSTS_FAIL":
-      return { ...state, error: action.payload, loading: false };
+      return { ...state, errorPosts: action.payload, loadingPosts: false };
+    case "USERS_REQUEST":
+      return { ...state, loadingUsers: true };
+    case "USERS_SUCCESS":
+      return {
+        ...state,
+        loadingUsers: false,
+        users: action.payload,
+        errorUsers: "",
+      };
+    case "USERS_FAIL":
+      return { ...state, errorUsers: action.payload, loadingUsers: false };
     default:
       return state;
   }
 };
 export default function HomePage() {
   const [state, dispatch] = useReducer(reducer, {
-    loading: false,
-    error: "",
+    loadingPosts: false,
+    errorPosts: "",
     posts: [],
+    loadingUsers: false,
+    errorUsers: "",
+    users: [],
   });
-  const { loading, error, posts } = state;
+  const { loadingPosts, errorPosts, posts, loadingUsers, errorUsers, users } =
+    state;
   const loadPosts = async () => {
     dispatch({ type: "POSTS_REQUEST" });
     try {
@@ -31,27 +52,56 @@ export default function HomePage() {
       dispatch({ type: "POSTS_FAIL", payload: err.message });
     }
   };
-
+  const loadUsre = async () => {
+    dispatch({ type: "USERS_REQUEST" });
+    try {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      dispatch({ type: "USERS_SUCCESS", payload: data });
+    } catch (err) {
+      dispatch({ type: "USERS_FAIL", payload: err.message });
+    }
+  };
   useEffect(() => {
     loadPosts();
+    loadUsre();
   }, []);
   return (
     <div className="blog">
       <div className="content">
         <h1>Posts</h1>
-        {loading ? (
+        {loadingPosts ? (
           <div>Loading...</div>
-        ) : error ? (
-          <div>Error:{error}</div>
+        ) : errorPosts ? (
+          <div>Error:{errorPosts}</div>
         ) : posts.length === 0 ? (
           <div>No post found</div>
         ) : (
           <ul>
             {posts.map((post) => (
               <li key={post.id}>
-                <h2>{post.title}</h2>
+                <Link to={`/post/${post.id}`}>
+                  <h2>{post.title}</h2>
+                </Link>
                 <p>{post.body}</p>
               </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div className="Sidebar">
+        <h2>Authors</h2>
+        {loadingUsers ? (
+          <div>Loading...</div>
+        ) : errorUsers ? (
+          <div>Error:{errorUsers}</div>
+        ) : users.length === 0 ? (
+          <div>No user found</div>
+        ) : (
+          <ul>
+            {users.map((user) => (
+              <li key={user.id}>{user.name}</li>
             ))}
           </ul>
         )}
