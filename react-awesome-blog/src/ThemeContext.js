fefffb0e544d615f8492f2b3caa8dd99b2bdea70
hghanbari <gh.hamid89@gmail.com>
2023-07-10 19:04:48 +0200

    Persist Setting for Theme

--- react-awesome-blog/src/ThemeContext.js
+++ react-awesome-blog/src/ThemeContext.js
@@ -4,11 +4,16 @@ const ThemeContext = createContext();
 const lsUser = localStorage.getItem("user")
   ? JSON.parse(localStorage.getItem("user"))
   : null;
+const lsTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : "light";
 function ThemeContextProvider(props) {
   const [user, setUser] = useState(lsUser);
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(lsTheme);
+
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
+    localStorage.setItem("theme", theme === "light" ? "dark" : "light");
   };
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme, user, setUser }}>
